6c41ddf2ecedaaf13932c24f4e45d4f568ca5381
Karim Slimani <karim@playmedia.fr>
2017-03-09 14:01:47 +0100

    Initial commit - Base Webpack v2 skeleton.

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Application bundle file
+import Clappr from 'clappr'
+import ChromecastPlugin from 'clappr-chromecast-plugin'
+
+console.log('App loaded !')
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+// Webpack configuration
+const path = require('path')
+const webpack = require('webpack')
+const NotifierPlugin = require('webpack-notifier')
+
+var outputFile, plugins = []
+
+if (process.env.npm_lifecycle_event === 'dist') {
+  outputFile = 'app.min.js'
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    output: {
+      comments: false,
+    },
+  }))
+} else {
+  outputFile = 'app.js'
+}
+
+plugins.push(new NotifierPlugin({
+  title: outputFile,
+  alwaysNotify: true,
+  // contentImage: path.resolve(__dirname, 'path/to/image.png')
+}))
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFile,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [
+          path.resolve(__dirname, 'src')
+        ],
+        options: {
+          presets: ['es2015'],
+        },
+      },
+    ],
+  },
+  plugins: plugins,
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+    // publicPath: '/js/',
+    compress: true,
+    host: "0.0.0.0",
+    port: 8080
+  }
+}
